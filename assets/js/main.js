e440333e728be94e8145fd954a39a103a3794377
akhmadt9 <akhmadt9@gmail.com>
2020-06-23 22:15:04 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+   let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+   bannerNode.parentNode.removeChild(bannerNode);
+}
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+$('.carousel').carousel({
+  interval: 2500
+})
+
+$(function () {
+  $("#mdb-lightbox-ui").load("./assets/mdb-addons/mdb-lightbox-ui.html");
+});
+
+$( document ).ready(function() {
+  new WOW().init();
+
+function animateValue(id, start, end, duration) {
+  let range = end - start;
+  let current = start;
+  let increment = end > start? 1 : -1;
+  let stepTime = Math.abs(Math.floor(duration / range));
+  let obj = document.getElementById(id);
+  let timer = setInterval(function() {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
+}
+
+setTimeout(function () {
+  animateValue("value", 0, 70, 1000);            
+}, 1000);
+
+});
